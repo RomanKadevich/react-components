0921bf9a3aa975d83d7778b71b4f0c45508f62be
Роман Ю. Кадевич <R.Kadevich@asud.kis.minsk>
2023-10-25 11:35:41 +0300

    feat: add input handler

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 export class Header extends Component {
+  state = {
+    value: "",
+  };
+  handleInput = (event: React.FormEvent<HTMLInputElement>) => {
+    event.preventDefault;
+    this.setState({ value: event.currentTarget.value });
+  };
   render() {
     return (
       <header className="p-16 bg-neutral-900">
@@ -8,8 +15,10 @@ export class Header extends Component {
           <input
             className="p-4 w-64 h-8"
             placeholder="Please enter your request"
+            value={this.state.value}
+            onChange={this.handleInput}
           ></input>
-          <button className="w-8 h-8 bg-transporent bg-cover bg-[url('./assets/search.svg')] hover:scale-90 hover:opacity-75 transition-opacity transition-transform ease-in-out duration-300"></button>
+          <button className="w-8 h-8 bg-transparent bg-cover bg-[url('./assets/search.svg')] hover:scale-90 transition-opacity transition-transform ease-in-out duration-300"></button>
         </div>
       </header>
     );
