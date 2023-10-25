a8878b76927b147c3f340a70407566bcb85782e3
Роман Ю. Кадевич <R.Kadevich@asud.kis.minsk>
2023-10-25 12:50:15 +0300

    feat: add props of header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Component } from "react";
 import Header from "./components/header";
+import { HandlerInputType } from "./types/types";
 
 export class App extends Component {
+  state = {
+    value: "",
+  };
+  handleInput: HandlerInputType = (event) => {
+    this.setState({ value: event.currentTarget.value });
+    console.log(this.state.value);
+  };
   render() {
     return (
       <>
-        <Header />
+        <Header handleInput={this.handleInput} value={this.state.value} />
       </>
     );
   }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import { Component } from "react";
+import { HandlerInputType } from "../types/types";
 
-export class Header extends Component {
-  state = {
-    value: "",
-  };
-  handleInput = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault;
-    this.setState({ value: event.currentTarget.value });
-  };
+export interface IHeader {
+  handleInput: HandlerInputType;
+  value: string;
+}
+
+export class Header extends Component<IHeader> {
   render() {
     return (
       <header className="p-16 bg-neutral-900">
@@ -15,8 +14,8 @@ export class Header extends Component {
           <input
             className="p-4 w-64 h-8"
             placeholder="Please enter your request"
-            value={this.state.value}
-            onChange={this.handleInput}
+            defaultValue={this.props.value}
+            onChange={this.props.handleInput}
           ></input>
           <button className="w-8 h-8 bg-transparent bg-cover bg-[url('./assets/search.svg')] hover:scale-90 transition-opacity transition-transform ease-in-out duration-300"></button>
         </div>
