f5858005307ffd87a9abd6141296cd340636f9b7
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-11-01 23:57:27 +0300

    feat: add header func with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/header";
 import {
   HandlerInputType,
@@ -6,7 +6,7 @@ import {
   IAnimal,
   IAnimals,
 } from "./types/types";
-import { List } from "./components/gallery";
+import { List } from "./components/List";
 
 interface IAppState {
   value: string;
@@ -14,94 +14,115 @@ interface IAppState {
   error: Error | null;
   isLoading: boolean;
 }
-const API_BASE_URL = "https://stapi.co/api/v1/rest/animal/search";
+const API_BASE_URL = "https://stapi.co/api/v1/rest/animal/search/";
 const PAGE_SIZE = 12;
 
-export class App extends Component {
-  state: IAppState = {
+export const App = () => {
+  const initialState: IAppState = {
     value: "",
     data: [],
     error: null,
-    isLoading: false
+    isLoading: false,
   };
-  componentDidMount = async () => {
-    try {
-      this.setState({isLoading:true})
-      const lastSearchData: string | null = localStorage.getItem("lastSearch");
-      const lastQueryData: string | null = localStorage.getItem("lastQuery");
-      if (lastSearchData) {
-        const lastSearch: IAnimal[] = JSON.parse(lastSearchData);
-        this.setState({ data: lastSearch, error: null });
-        if (lastQueryData) {
-          this.setState({ value: lastQueryData, error: null });
-        }
-      } else {
-        const response = await fetch(
-          `${API_BASE_URL}?pageNumber=1&pageSize=${PAGE_SIZE}`,
-        );
-        
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const animals: IAnimals = await response.json();
-        this.setState({isLoading:false})
-        this.setState({ data: animals.animals, error: null });
-      }
-    } catch (error) {
-      this.setState({ error, data: [] });
-    }
-  };
-  handleInputSearch: HandlerInputType = (event) => {
-    this.setState({ value: event.currentTarget.value });
-  };
-  handleSubmitSearch: HandlerSubmitType = async (event) => {
-    event.preventDefault();
-    try {
-      this.setState({isLoading:true})
-      const response = await fetch(
-        `${API_BASE_URL}?name=${this.state.value}&pageSize=${PAGE_SIZE}`,
+  const [state, setState] = useState(initialState);
+  const getData = async (value?: string) => {
+    let response: Response | undefined = undefined;
+    if (!value) {
+      response = await fetch(
+        `${API_BASE_URL}?pageNumber=1&pageSize=${PAGE_SIZE}`,
+      );
+    } else {
+      response = await fetch(
+        `${API_BASE_URL}?name=${value}&pageSize=${PAGE_SIZE}`,
         {
           method: "POST",
         },
       );
+    }
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const animals: IAnimals = await response.json();
+    return animals;
+  };
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        setState((prevState) => ({ ...prevState, isLoading: true }));
+
+        const lastQueryData: string | null = localStorage.getItem("lastQuery");
+        if (lastQueryData) {
+       
+          const animals = await getData(lastQueryData);
       
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+          setState((prevState) => ({
+            ...prevState,
+            value: lastQueryData,
+            data: animals.animals,
+            error: null,
+            isLoading: false,
+          }));
+        } else {
+          const animals: IAnimals = await getData();
+          setState((prevState) => ({
+            ...prevState,
+            data: animals.animals,
+            error: null,
+            isLoading: false,
+          }));
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          setState((prev) => ({ ...prev, error: error as Error, data: [] }));
+        }
       }
-      const animals: IAnimals = await response.json();
-      this.setState({isLoading:false})
-      this.setState({ data: animals.animals, error: null });
+    };
+    loadData();
+  }, [setState]);
+  const handleInputSearch: HandlerInputType = (event) => {
+    setState({ ...state, value: event.currentTarget.value });
+  };
+  const handleSubmitSearch: HandlerSubmitType = async (event) => {
+    event.preventDefault();
+    try {
+      setState({ ...state, isLoading: true });
+
+      const animals: IAnimals = await getData(state.value);
+      setState({ ...state, isLoading: false });
+      setState({ ...state, data: animals.animals, error: null });
       localStorage.setItem("lastSearch", JSON.stringify(animals.animals));
-      localStorage.setItem("lastQuery", this.state.value);
+      localStorage.setItem("lastQuery", state.value);
     } catch (error) {
-      this.setState({ error, data: [] });
+      if (error instanceof Error) {
+        setState({ ...state, error, data: [] });
+      }
     }
   };
 
-  render() {
-    const { value, data, error, isLoading } = this.state;
-
-    return (
-      <>
-        {error ? (
-          <div className="bg-white p-3 text-center text-red-500 font-bold">
-            Error when loading data{error.message}
-          </div>
-        ) : null}
-        {data ? (
-          <>
-            <Header
-              handleInputSearch={this.handleInputSearch}
-              handleSubmitSearch={this.handleSubmitSearch}
-              value={value}
-            />
-            {isLoading&&<div className="flex justify-center items-center font-bold text-lg"><p>Loading...</p></div>}
-            <List animals={data} />
-          </>
-        ) : null}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {state.error ? (
+        <div className="bg-white p-3 text-center text-red-500 font-bold">
+          Error when loading data{state.error.message}
+        </div>
+      ) : null}
+      {state.data ? (
+        <>
+          <Header
+            handleInputSearch={handleInputSearch}
+            handleSubmitSearch={handleSubmitSearch}
+            value={state.value}
+          />
+          {state.isLoading && (
+            <div className="flex justify-center items-center font-bold text-lg">
+              <p>Loading...</p>
+            </div>
+          )}
+          <List animals={state.data} />
+        </>
+      ) : null}
+    </>
+  );
+};
 
 export default App;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useContext } from "react";
 import { HandlerInputType } from "../types/types";
 import { HandlerSubmitType } from "../types/types";
 import { ErrorBoundaryContext } from "./errorBoundary";
@@ -9,42 +9,41 @@ export interface IHeader {
   value: string;
 }
 
-export class Header extends Component<IHeader> {
-  render() {
-    const { handleInputSearch, handleSubmitSearch, value } = this.props;
+export const Header = ({
+  handleInputSearch,
+  handleSubmitSearch,
+  value,
+}: IHeader) => {
+  const contextData = useContext(ErrorBoundaryContext);
 
-    return (
-      <header className="p-16 bg-violet-950">
-        <div className="container mx-auto flex flex-wrap justify-center items-center gap-10">
-          <h1 className="font-bold text-white text-[1.5rem]">Animals</h1>
-          <input
-            className="p-4 w-64 h-8"
-            placeholder="Please enter your request"
-            value={value}
-            onChange={handleInputSearch}
-          ></input>
-          <button
-            onClick={handleSubmitSearch}
-            className="w-8 h-8 bg-transparent bg-cover bg-[url('./assets/search.svg')] hover:scale-90 transition-opacity transition-transform ease-in-out duration-300"
-          ></button>
-          <ErrorBoundaryContext.Consumer>
-            {(context) => (
-              <button
-                className="px-4 py-2 bg-violet-700 text-white rounded hover:bg-violet-600 ml-4"
-                onClick={() =>
-                  context
-                    ? context.triggerError(new Error("Test error"))
-                    : undefined
-                }
-              >
-                Test error
-              </button>
-            )}
-          </ErrorBoundaryContext.Consumer>
-        </div>
-      </header>
-    );
-  }
-}
+  return (
+    <header className="p-16 bg-violet-950">
+      <div className="container mx-auto flex flex-wrap justify-center items-center gap-10">
+        <h1 className="font-bold text-white text-[1.5rem]">Animals</h1>
+        <input
+          className="p-4 w-64 h-8"
+          placeholder="Please enter your request"
+          value={value}
+          onChange={handleInputSearch}
+        ></input>
+        <button
+          onClick={handleSubmitSearch}
+          className="w-8 h-8 bg-transparent bg-cover bg-[url('./assets/search.svg')] hover:scale-90 transition-opacity transition-transform ease-in-out duration-300"
+        ></button>
+
+        <button
+          className="px-4 py-2 bg-violet-700 text-white rounded hover:bg-violet-600 ml-4"
+          onClick={() =>
+            contextData
+              ? contextData.triggerError(new Error("Test error"))
+              : undefined
+          }
+        >
+          Test error
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
