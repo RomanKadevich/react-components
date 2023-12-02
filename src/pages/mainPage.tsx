0d2e40ab1b3f3262122c447b587bd168b7af353b
RomanKadevich <123977364+RomanKadevich@users.noreply.github.com>
2023-12-02 23:25:57 +0300

    feat: add main page view

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,35 +1,61 @@
+import { useEffect, useState } from "react";
 import { IData } from "../comonents/Form";
 import { useAppSelector } from "../store";
 
-
-
-
 const MainPage = () => {
-  const value:IData = useAppSelector((state) => state.unControlFormData.value);
-  return <div> 
-  <div>
-    <h2>Uncontrol form data</h2>
-    <ul>
-      <li>
-        Name: {value.Name}
-      </li>
-      <li>
-        Age: {value.Age}
-      </li>
-      <li>
-        Email: {value.Email}
-      </li>
-      <li>
-        Password: {value.Password}
-      </li>
-      <li>
-        Radio: {value.Gender}
-      </li>
-      <li>
-      Accept T&C: {value.checkbox.toString()}
-      </li>
-    </ul>
-  </div></div>;
+  const valueOfUncontrolForm: IData = useAppSelector(
+    (state) => state.unControlFormData.value,
+  );
+  const valueOfControlForm: IData = useAppSelector(
+    (state) => state.ControlFormData.value,
+  );
+  const [controlFormList, setControlFormList] = useState<IData[] | []>([]);
+  const [uncontrolFormList, setUncontrolFormList] = useState<IData[] | []>([]);
+
+  useEffect(() => {
+    setControlFormList((prev) => [...prev, valueOfControlForm]);
+  }, [valueOfControlForm]);
+
+  useEffect(() => {
+    setUncontrolFormList((prev) => [...prev, valueOfUncontrolForm]);
+  }, [valueOfUncontrolForm]);
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+      <div>
+        <h2>Control form data</h2>
+        {controlFormList.map((item, index, array) => (
+          <ul
+            className={array.length - 1 === index ? "higlight" : ""}
+            key={index + Date.now()}
+          >
+            <li>Name: {item.Name}</li>
+            <li>Age: {item.Age}</li>
+            <li>Email: {item.Email}</li>
+            <li>Password: {item.Password}</li>
+            <li>Radio: {item.Gender}</li>
+            <li>Accept T&C: {item.checkbox.toString()}</li>
+          </ul>
+        ))}
+      </div>
+      <div>
+        <h2>Uncontrol form data</h2>
+        {uncontrolFormList.map((item, index, array) => (
+          <ul
+            className={array.length - 1 === index ? "higlight" : ""}
+            key={index + Date.now()}
+          >
+            <li>Name: {item.Name}</li>
+            <li>Age: {item.Age}</li>
+            <li>Email: {item.Email}</li>
+            <li>Password: {item.Password}</li>
+            <li>Radio: {item.Gender}</li>
+            <li>Accept T&C: {item.checkbox.toString()}</li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
